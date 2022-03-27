0b919e13a71abad606d21f38ad23cbc616a6f39d
Rudolf Harmse <rudolf.harmse@gmail.com>
2022-03-27 14:30:10 +0200

    Added base project

---
+++ src/test/java/net/scipher7/sfgdi/SfgDiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.scipher7.sfgdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

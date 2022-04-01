c58ebad04f98af91595b318b63a282b69e980139
Rudolf Harmse <rudolf.harmse@gmail.com>
2022-04-01 09:22:19 +0200

    Fixing the best animal service

--- src/main/java/guru/springframework/sfgdi/services/I18NSpanishService.java
+++ src/main/java/guru/springframework/sfgdi/services/I18NSpanishService.java
@@ -1,5 +1,6 @@
 package guru.springframework.sfgdi.services;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 

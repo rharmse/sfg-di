cb651da6b0493dc4d8a9f0e6bd32faa758feb0e8
Rudolf Harmse <rudolf.harmse@gmail.com>
2022-04-01 09:25:15 +0200

    Removed unused import

--- src/main/java/guru/springframework/sfgdi/services/I18nEnglishGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/I18nEnglishGreetingService.java
@@ -1,6 +1,5 @@
 package guru.springframework.sfgdi.services;
 
-import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
